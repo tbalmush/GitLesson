d0f30602fec361f6e17056ae111ffa909d1a69ff
Tatiana <tabalmush@gmail.com>
2022-04-27 20:35:02 +0300

    Tets commit a

--- src/src/main/java/Main.java
+++ src/src/main/java/Main.java
@@ -1,11 +1,27 @@
-public class Variables {
+import java.sql.SQLOutput;
+
+public class Main {
 
   public static void main(String[] args) {
-    int number = 10;
-    number = 11;
-    System.out.println(number);
+    int one = 1;
+    int two = 2;
+    int three = 3;
+    int four = 4;
+    boolean type = true;
+    float floatNumber = 12.456F;
+    char letter = 'a';
+    char sun = '\u2600';
 
+    byte myByte = 123;
+    double myDouble = myByte;
+    System.out.println(myDouble);
+    System.out.println();
+
+    System.out.println(one + " " + two + " " + three + " " + four);
+    System.out.println(sun);
+    System.out.println(type);
+    System.out.println(floatNumber);
+    System.out.println(letter);
 
   }
-
 }
